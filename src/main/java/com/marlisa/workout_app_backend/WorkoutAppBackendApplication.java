5221a521c4757665e0ed573e2d5caf6c3b19c336
Marlisa Odokara <marlisa.od@gmail.com>
2024-06-30 16:58:07 -0500

    create env file

--- src/main/java/com/marlisa/workout_app_backend/WorkoutAppBackendApplication.java
+++ src/main/java/com/marlisa/workout_app_backend/WorkoutAppBackendApplication.java
@@ -1,10 +1,13 @@
 package com.marlisa.workout_app_backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WorkoutAppBackendApplication {
+
+	private Dotenv dotenv;
 
 	public static void main(String[] args) {
 		SpringApplication.run(WorkoutAppBackendApplication.class, args);
